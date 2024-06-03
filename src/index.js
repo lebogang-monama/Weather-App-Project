cc518ce9a37b3cd3947df90174f4d206627eaf42
lebogang-monama <165066877+lebogang-monama@users.noreply.github.com>
2024-06-04 00:23:40 +0200

    Added degrees farenheit temperature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function Weather(response) {
-let currentTemperature = document.querySelector("#current-temperature");
+let currentTemperature = document.querySelector("#degrees-celsius");
+let farenheit = document.querySelector("#degrees-farenheit");
 let temperature = Math.round(response.data.temperature.current);
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement =  document.querySelector("#wind");
 
-console.log(response.data.wind.speed);
-
 currentTemperature.innerHTML = temperature;
+farenheit.innerHTML = Math.round((temperature*9/5)+32);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = response.data.temperature.humidity;
 windElement.innerHTML = response.data.wind.speed;
@@ -15,7 +15,7 @@ windElement.innerHTML = response.data.wind.speed;
 
 function searchCity(city) {
   let apiKey = "082e132ob80f941b2a0b6afd02a0td4e";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(Weather);
 }
 
